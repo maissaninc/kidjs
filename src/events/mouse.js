93e949b17e47f3f4b23a3addd67a5d3c08a0a6ff
Chris Maissan <chris@maissan.net>
2021-03-25 19:05:36 -0400

    Dispatch swipe events

--- src/events/mouse.js
+++ src/events/mouse.js
@@ -1,16 +1,44 @@
+const SWIPE_MIN_DISTANCE = 150;
+const SWIPE_CROSS_MAX_DISTANCE = 100;
+const SWIPE_MAX_TIME = 200;
+
 let x = false;
 let y = false;
-let time = false;
+let t = false;
 
 function onMouseDown(e) {
   x = e.clientX;
   y = e.clientY;
-  time = Date.now();
+  t = Date.now();
   window.mouseButton = e.button == 2 ? 'right' : 'left'
 }
 
 function onMouseUp(e) {
   window.mouseButton = false;
+
+  // Check for swipe
+  if (x !== false && y !== false && time !== false) {
+    let deltaX = e.clientX - x;
+    let deltaY = e.clientY - y;
+    let deltaT = Date.now() - t;
+
+    if (deltaT < SWIPE_MAX_TIME) {
+      if (deltaX > SWIPE_MIN_DISTANCE && Math.abs(deltaY) < SWIPE_CROSS_MAX_DISTANCE) {
+        window.dispatchEvent(new CustomEvent('swiperight'));
+      }
+      if (deltaX < -SWIPE_MIN_DISTANCE && Math.abs(deltaY) < SWIPE_CROSS_MAX_DISTANCE) {
+        window.dispatchEvent(new CustomEvent('swipeleft'));
+      }
+      if (deltaY > SWIPE_MIN_DISTANCE && Math.abs(deltaX) < SWIPE_CROSS_MAX_DISTANCE) {
+        window.dispatchEvent(new CustomEvent('swipedown'));
+      }
+      if (deltaY < -SWIPE_MIN_DISTANCE && Math.abs(deltaX) < SWIPE_CROSS_MAX_DISTANCE) {
+        window.dispatchEvent(new CustomEvent('swipeup'));
+      }
+    }
+  }
+
+  t = false;
 }
 
 function onMouseMove(e) {

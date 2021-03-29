6e53f5d80e1f93211d3c25a2fe238d2661bbbfef
Chris Maissan <chris@maissan.net>
2021-03-29 16:59:05 -0400

    Fleshed out Vector class

--- src/shape/vector.js
+++ src/shape/vector.js
@@ -10,6 +10,31 @@ export default class Vector {
     );
   }
 
+  add(v) {
+    return new Vector(this.x + v.x, this.y + v.y);
+  }
+
+  subtract(v) {
+    return new Vector(this.x - v.x, this.y - v.y);
+  }
+
+  scale(s) {
+    return new Vector(this.x * s, this.y * s);
+  }
+
+  normalize() {
+    let l = this.length;
+    return l > 0 ? new Vector(this.x / l, this.y / l) : new Vector(0, 0);
+  }
+
+  dot(v) {
+    return (this.x * v.x + this.y * v.y);
+  }
+
+  cross(v) {
+    return (this.x * v.y - this.y * v.x);
+  }
+
   rotate(deg) {
     let angle = deg * (Math.PI / 180);
     return new Vector(
@@ -18,14 +43,9 @@ export default class Vector {
     );
   }
 
-  normalize() {
-    return new Vector(
-      this.x / this.length,
-      this.y / this.length
+  distance(v) {
+    return Math.sqrt(
+      Math.pow(this.x - v.x, 2) + Math.pow(this.y - v.y, 2)
     );
-  }
-
-  dotProduct(v) {
-    return (this.x * v.x + this.y * v.y);
   }
 }

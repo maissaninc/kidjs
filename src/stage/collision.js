a6eda7fa116468080d591ff0b915285505e56fa9
Chris Maissan <chris@maissan.net>
2021-04-06 17:07:21 -0400

    Adjust angular velocities on collision

--- src/stage/collision.js
+++ src/stage/collision.js
@@ -19,6 +19,10 @@ export default class Collision {
     this.normal = normal;
     this.start = start;
   }
+
+  get end() {
+    return this.start.add(this.normal.scale(this.depth));
+  }
 }
 
 /**
@@ -44,8 +48,29 @@ export function resolveCollision(collision) {
     v.scale(collision.b.inverseMass)
   );
 
+  // Determine point of collision
+  let start = collision.start.scale(
+    collision.b.inverseMass / (collision.a.inverseMass + collision.b.inverseMass)
+  );
+  let end = collision.end.scale(
+    collision.a.inverseMass / (collision.a.inverseMass + collision.b.inverseMass)
+  );
+  let p = start.add(end);
+  let r1 = p.subtract(collision.a.position);
+  let r2 = p.subtract(collision.b.position);
+
+  // Determine linear velocities at point of collision
+  let v1 = collision.a.velocity.add(new Vector(
+    -1 * collision.a.angularVelocity * r1.y,
+    collision.a.angularVelocity * r1.x
+  ));
+  let v2 = collision.b.velocity.add(new Vector(
+    -1 * collision.b.angularVelocity * r2.y,
+    collision.b.angularVelocity * r2.x
+  ));
+
   // Calculate relative velocities
-  let relativeVelocity = collision.b.velocity.subtract(collision.a.velocity);
+  let relativeVelocity = v2.subtract(v1);
   let relativeVelocityInNormal = relativeVelocity.dot(collision.normal);
 
   // Objects are already moving apart
@@ -54,8 +79,16 @@ export function resolveCollision(collision) {
   // Apply impulse along normal
   let bounciness = Math.min(collision.a.bounciness, collision.b.bounciness);
   let friction = Math.min(collision.a.friction, collision.b.friction);
+  let r1CrossNormal = r1.cross(collision.normal);
+  let r2CrossNormal = r2.cross(collision.normal);
   let jN = -(1 + bounciness) * relativeVelocityInNormal;
-  jN = jN / (collision.a.inverseMass + collision.b.inverseMass);
+  jN = jN / (
+    collision.a.inverseMass + collision.b.inverseMass +
+    Math.pow(r1CrossNormal, 2) * collision.a.inertia +
+    Math.pow(r2CrossNormal, 2) * collision.b.inertia
+  );
+
+  // Adjust linear velocity
   collision.a.velocity = collision.a.velocity.subtract(
     collision.normal.scale(jN * collision.a.inverseMass)
   );
@@ -63,18 +96,34 @@ export function resolveCollision(collision) {
     collision.normal.scale(jN * collision.b.inverseMass)
   );
 
+  // Adjust rotational velocity
+  collision.a.angularVelocity = collision.a.angularVelocity - (r1CrossNormal * jN * collision.a.inertia);
+  collision.b.angularVelocity = collision.b.angularVelocity + (r2CrossNormal * jN * collision.b.inertia);
+
   // Apply impulse along tangent
   let tangent = relativeVelocity.subtract(
     collision.normal.scale(relativeVelocity.dot(collision.normal))
   );
   tangent = tangent.normalize().scale(-1);
+  let r1CrossTangent = r1.cross(tangent);
+  let r2CrossTangent = r2.cross(tangent);
   let jT = -(1 + bounciness) * relativeVelocity.dot(tangent) * friction;
-  jT = jT / (collision.a.inverseMass + collision.b.inverseMass);
+  jT = jT / (
+    collision.a.inverseMass + collision.b.inverseMass +
+    Math.pow(r1CrossTangent, 2) * collision.a.inertia +
+    Math.pow(r2CrossTangent, 2) * collision.b.inertia
+  );
   jT = Math.min(jT, jN);
+
+  // Adjust linear velocity
   collision.a.velocity = collision.a.velocity.subtract(
     tangent.scale(jT * collision.a.inverseMass)
   );
   collision.b.velocity = collision.b.velocity.add(
     tangent.scale(jT * collision.b.inverseMass)
   );
+
+  // Adjust angular velocity
+  collision.a.angularVelocity = collision.a.angularVelocity - (r1CrossTangent * jT * collision.a.inertia);
+  collision.b.angularVelocity = collision.b.angularVelocity + (r2CrossTangent * jT * collision.b.inertia);
 }

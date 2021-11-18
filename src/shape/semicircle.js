fc24c88eaa27a6712cd6d595cd0f873a0d591435
Chris Maissan <chris@maissan.net>
2021-11-18 12:34:14 -0500

    Create body for semi circle

--- src/shape/semicircle.js
+++ src/shape/semicircle.js
@@ -1,9 +1,25 @@
 import Shape from './';
+import Vector from '../core/vector';
+import Matter from 'matter-js';
 
 export default class Semicircle extends Shape {
   constructor(x, y, radius) {
     super(x, y);
     this.radius = radius;
+  }
+
+  init() {
+    let points = [];
+    for (let theta = 0; theta <= Math.PI; theta = theta + 0.1) {
+      points.push(new Vector(
+        Math.cos(theta) * this.radius,
+        Math.sin(theta) * this.radius
+      ));
+    }
+    this.body =  Matter.Bodies.fromVertices(this.position.x, this.position.y, points, {
+      frictionAir: 0,
+      isStatic: true
+    });
   }
 
   render(context) {
@@ -19,6 +35,7 @@ export function semicircle(x, y, diameter) {
     return;
   }
   const shape = new Semicircle(x, y, diameter / 2);
+  shape.init();
   window.stage.addChild(shape);
   return shape;
 }

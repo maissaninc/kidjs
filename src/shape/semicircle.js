8a04a4e08132bbb3abcb4fc3e7a396d0117e40c6
Chris Maissan <chris@maissan.net>
2022-05-05 14:12:31 -0400

    Explode semicircle

--- src/shape/semicircle.js
+++ src/shape/semicircle.js
@@ -1,4 +1,5 @@
 import Shape from './';
+import Pie from './pie';
 import Vector from '../core/vector';
 import Matter from 'matter-js';
 import { parseLength } from '../core/units';
@@ -33,6 +34,55 @@ export default class Semicircle extends Shape {
     );
     context.arc(this.x + offset.x, this.y + offset.y, this.radius, angleRadians, angleRadians + Math.PI);
     this.postrender(context);
+  }
+
+  explode() {
+
+    // Don't explode twice
+    if (this.exploded) return;
+    this.exploded = true;
+
+    // Break into 6 pie pieces
+    let fragments = [];
+    for (let angle = this.angle; angle < this.angle + Math.PI; angle = angle + Math.PI * 1/3) {
+
+      // Create new fragment
+      let points = [
+        new Vector(0, 0),
+        new Vector(
+          Math.cos(angle) * this.radius,
+          Math.sin(angle) * this.radius
+        ),
+        new Vector(
+          Math.cos(angle + Math.PI * 1/3) * this.radius,
+          Math.sin(angle + Math.PI * 1/3) * this.radius
+        )
+      ];
+      let cp = Vector.average(points);
+      let fragment = new Pie(
+        this.x + cp.x,
+        this.y + cp.y,
+        this.radius,
+        angle,
+        angle + Math.PI * 1/3
+      );
+
+      // Add to stage
+      fragment.init();
+      fragment.fill = this.fill;
+      fragment.stroke = this.stroke;
+      window.stage.addChild(fragment);
+
+      // Push away from origin
+      let n = cp.normalize();
+      fragment.push(n.x * 5, n.y * 5);
+      fragment.angularVelocity = Math.random() * 5;
+      fragments.push(fragment);
+    }
+
+    // Remove self from stage
+    this.remove();
+    this._fragments = fragments;
   }
 
   /**

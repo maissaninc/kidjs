98dabf9add625092c76a53b7fb10f1588741bc7a
Chris Maissan <chris@maissan.net>
2022-10-25 19:11:29 -0400

    "move" event

--- src/stage/index.js
+++ src/stage/index.js
@@ -230,7 +230,16 @@ export default class Stage {
 
       // Render actors
       for (let actor of this.actors) {
+
+        // If non-zero velocity, fire move event
+        if (actor.body && actor.body.velocity.x != 0 && actor.body.velocity.y != 0) {
+          actor.dispatchEvent(new CustomEvent('move'));
+        }
+
+        // Update position
         actor.update();
+
+        // Render
         if (!actor.invisible) {
           actor.render(this.context);
         }

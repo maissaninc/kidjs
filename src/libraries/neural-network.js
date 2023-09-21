a167ffbb49c2f0e3ad0d0aa44f29bd3e14f6008f
Chris Maissan <chris@maissan.net>
2023-09-21 17:11:07 -0400

    Normalize neural network inputs

--- src/libraries/neural-network.js
+++ src/libraries/neural-network.js
@@ -9,23 +9,66 @@ export class NeuralNetwork {
    */
   constructor() {
     this.nn = new Brain.NeuralNetwork();
+    this._trainingData = [];
+    this._needsTraining = true;
+    this._inputRanges = [];
+    this._outputRanges = [];
   }
 
   _parseInput(input) {
-    if (typeof input == 'string') {
-      return input.split(' ');
+
+    // Single number
+    if (typeof input == 'number') {
+      input = [input];
     }
-    if (Array.isArray(input)) {
-      return input;
-    }
-    return [input];
+
+    return input;
   }
 
-  _parseOutput(output) {
-    if (Array.isArray(output)) {
-      return output;
+  _parseOutput(output) { 
+    return output;
+  }
+
+  _normalizeData(data) {
+    let result = [];
+    let minimums = [];
+    let maximums = [];
+
+    // Determine minimum and maximum values for each input
+    for (let i = 0; i < data.length; i = i + 1) {
+      for (let j in data[i].input) {
+        if (!minimums[j] || !maximums[j]) {
+          minimums[j] = 0;
+          maximums[j] = 1;
+        }
+        if (data[i].input[j] < minimums[j]) {
+          minimums[j] = data[i].input[j];
+        }
+        if (data[i].input[j] > maximums[j]) {
+          maximums[j] = data[i].input[j];
+        }
+      }
     }
-    return [output];
+
+    // Calculate size of each range
+    let size = [];
+    for (let i in minimums) {
+      size[i] = maximums[i] - minimums[i];
+    }
+
+    // Normalize all inputs to be between 0-1
+    for (let i = 0; i < data.length; i = i + 1) {
+      let normalized = {
+        input: [],
+        output: data[i].output
+      };
+      for (let j in data[i].input) {
+        normalized.input[j] = (data[i].input[j] + minimums[j]) / size[j];
+      }
+      result.push(normalized);
+    }
+
+    return result;
   }
 
   /**
@@ -34,6 +77,12 @@ export class NeuralNetwork {
    * @param {*} input
    */
   run(input) {
+    if (this._needsTraining) {
+      let data = this._normalizeData(this._trainingData);
+      console.log(data);
+      this.nn.train(data);
+      this._needsTraining = false;
+    }
     let result = this.nn.run(this._parseInput(input));
     return result;
   }
@@ -45,10 +94,11 @@ export class NeuralNetwork {
    * @param {*} output
    */
   train(input, output) {
-    this.nn.train([{
+    let data = {
       input: this._parseInput(input),
       output: this._parseOutput(output)
-    }]);
+    };
+    this._trainingData.push(data);
   }
 }
 

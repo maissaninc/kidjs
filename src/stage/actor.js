3ce1576b24dd3315a077d3b08ddef6d5d98e9d2a
Chris Maissan <chris@maissan.net>
2025-11-16 18:11:53 -0500

    Allow velocity vector to be set

--- src/stage/actor.js
+++ src/stage/actor.js
@@ -36,11 +36,11 @@ export default class Actor {
     }
 
     // Detect change in velocity
-    this.velocity = new Vector(0, 0);
-    this.velocity.onchange = () => {
+    this._velocity = new Vector(0, 0);
+    this._velocity.onchange = () => {
       this.anchored = false;
       if (this.body) {
-        Matter.Body.setVelocity(this.body, this.velocity);
+        Matter.Body.setVelocity(this.body, this._velocity);
       }
     }
 
@@ -104,12 +104,10 @@ export default class Actor {
   }
 
   set acceleration(value) {
-    console.log('Set');
     if (value instanceof Vector) {
       this._acceleration.x = value.x;
       this._acceleration.y = value.y;
     } else {
-      console.log('Dir' + this.direction);
       this._acceleration.x = Math.cos(degreesToRadians(this.direction)) * value;
       this._acceleration.y = Math.sin(degreesToRadians(this.direction)) * value;
     }
@@ -294,6 +292,20 @@ export default class Actor {
       return v.length;
     }
     return 0;
+  }
+
+  get velocity() {
+    return this._acceleration;
+  }
+
+  set velocity(value) {
+    if (value instanceof Vector) {
+      this._velocity.x = value.x;
+      this._velocity.y = value.y;
+    } else {
+      this._velocity.x = 0;
+      this._velocity.y = 0;
+    }
   }
 
   /**

1e4434229c66f5056b485de2f21198e39d676cb3
Chris Maissan <chris@maissan.net>
2026-09-19 11:59:57 -0400

    Added width and height to settings

--- src/core/settings.js
+++ src/core/settings.js
@@ -7,66 +7,92 @@ export class Settings {
     _grid = false;
     _pixelSize = 1;
     _orientation = 'auto';
+    _width = 'auto';
+    _height = 'auto';
 
    set backgroundColor(value) {
-    this._backgroundColor = value;
+     this._backgroundColor = value;
    }
 
    get backgroundColor() {
-    return this._backgroundColor;
+     return this._backgroundColor;
    }
 
    set caseInsensitive(value) {
-    this._caseInsensitive = value;
+     this._caseInsensitive = value;
    }
 
    get caseInsensitive() {
-    return this._caseInsensitive;
+     return this._caseInsensitive;
    }
 
    set slowMotion(value) {
-    this._slowMotion = value;
+     this._slowMotion = value;
    }
 
    get slowMotion() {
-    return this._slowMotion;
+     return this._slowMotion;
    }
 
    set slowMotionDelay(value) {
-    this._slowMotionDelay = value;
+     this._slowMotionDelay = value;
    }
 
    get slowMotionDelay() {
-    return this._slowMotionDelay;
+     return this._slowMotionDelay;
    }
 
    set grid(value) {
-    this._grid = value;
-    if (window.grid) {
-      window.stage.resize();
-    }
+     this._grid = value;
+     if (window.grid) {
+       window.stage.resize();
+     }
    }
 
    get grid() {
-    return this._grid;
+     return this._grid;
    }
 
    set pixelSize(value) {
-    this._pixelSize = value;
-    if (window.stage) {
-      window.stage.resize();
-    }
+     this._pixelSize = value;
+     if (window.stage) {
+       window.stage.resize();
+     }
    }
 
    get pixelSize() {
-    return this._pixelSize;
+     return this._pixelSize;
    }
 
    set orientation(value) {
-    this._orientation = value;
+     this._orientation = value;
    }
 
    get orientation() {
-    return this._orientation;
+     return this._orientation;
+   }
+
+   set width(value) {
+     if (parseInt(value)) {
+       this._width = parseInt(value);
+     } else {
+       this._width = 'auto';
+     }
+   }
+
+   get width() {
+     return this._width;
+   }
+
+   set height(value) {
+     if (parseInt(value)) {
+       this._height = parseInt(value);
+     } else {
+       this._height = 'auto';
+     }
+   }
+
+   get height() {
+     return this._height;
    }
 }
